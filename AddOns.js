e76be3aca20c1cb0bb2865d1c0b41b364b9c6adf
Frank Hui <faihui_frank@yahoo.com>
2017-06-09 12:38:27 -0700

    Create AddOns.js

---
+++ AddOns.js
@@ -0,0 +1,88 @@
+(function () {
+    "use strict";
+    angular.module(APPNAME).factory('productService', ProductService);
+    ProductService.$inject = ['$baseService', '$sabio'];
+
+    function ProductService($baseService, $sabio) {
+        var prodServiceObj = sabio.services.product;
+        var newService = $baseService.merge(true, {}, prodServiceObj, $baseService);
+        return newService;
+    }
+})();
+
+//routing
+(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+       .config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+
+           $locationProvider.hashPrefix('!');
+
+           $routeProvider.when('/', {
+               templateUrl: '/Scripts/app/templates/join/addons.html',
+               controller: 'addOnController',
+               controllerAs: 'addon'
+           }).when('/lashpick', {
+               templateUrl: '/Scripts/app/templates/join/lashpick.html',
+               controller: 'lashPickController',
+               controllerAs: 'lash'
+           }).when('/healthPick', {
+               templateUrl: '/Scripts/app/templates/join/healthPick.html',
+               controller: 'healthController',
+               controllerAs: 'health'
+           })
+               .when('/shipping', {
+               templateUrl: '/Scripts/app/templates/join/shipping.html',
+               controller: 'joinController',
+               controllerAs: 'join'
+           }).when('/summary', {
+               templateUrl: '/Scripts/app/templates/join/summary.html',
+               controller: 'summaryController',
+               controllerAs: 'summary'
+           });
+
+           $locationProvider.html5Mode({
+               enabled: false,
+               requireBase: false
+           });
+
+       }]);
+
+
+})();
+
+(function () {
+    "user strict";
+    angular.module(APPNAME).controller('joinHeaderController', JoinHeaderController);
+
+    JoinHeaderController.$inject = ['$scope', '$window', '$baseController'];
+
+    function JoinHeaderController($scope, $window, $baseController) {
+        var vm = this;
+
+        vm.$scope = $scope;
+        $baseController.merge(vm, $baseController);
+        vm.$window = $window;
+
+        vm.header = "Choose Your Add-Ons"
+
+        vm.$onInit = _render;
+
+        function _render() {
+            vm.$systemEventService.listen('addon', _onSystemEvent);
+            vm.$systemEventService.listen('healthPick', _onSystemEvent);
+            vm.$systemEventService.listen('lashpick', _onSystemEvent);
+            vm.$systemEventService.listen('join', _onSystemEvent);
+        }
+
+        function _onSystemEvent(event, payload) {
+            vm.header = payload[1];
+        }
+    }
+
+})();
+
+
+
+
